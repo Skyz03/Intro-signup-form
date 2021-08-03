5e0d261b44db7de782d02aae31b5dfdf466979ca
Syz03 <aakibdiscoo@gmail.com>
2021-08-03 20:40:19 +0545

    Updates in the code with the required input validation

--- index.js
+++ index.js
@@ -1,61 +1,6 @@
 
 const form = document.getElementsByTagName('form')[0];
 
-// const email = document.getElementById('mail');
-// const emailError = document.querySelector('#mail + .error');
-
-
-// email.addEventListener('input', function (event) {
-//     // Each time the user types something, we check if the
-//     // form fields are valid.
-
-//     if (email.validity.valid) {
-//         // In case there is an error message visible, if the field
-//         // is valid, we remove the error message.
-//         emailError.textContent = ''; // Reset the content of the message
-//         emailError.className = 'error'; // Reset the visual state of the message
-//     } else {
-//         // If there is still an error, show the correct error
-//         showError();
-//     }
-// });
-
-// form.addEventListener('submit', function (event) {
-//     // if the email field is valid, we let the form submit
-
-//     if (!email.validity.valid) {
-//         // If it isn't, we display an appropriate error message
-//         showError();
-//         // Then we prevent the form from being sent by canceling the event
-//         event.preventDefault();
-//     }
-// });
-
-// function showError() {
-
-//     if (email.validity.valueMissing) {
-//         // If the field is empty,
-//         // display the following error message.
-//         emailError.textContent = 'You need to enter an e-mail address.';
-//     } else if (email.validity.typeMismatch) {
-//         // If the field doesn't contain an email address,
-//         // display the following error message.
-//         emailError.textContent = 'Entered value needs to be an e-mail address.';
-//     } else if (email.validity.tooShort) {
-//         // If the data is too short,
-//         // display the following error message.
-//         emailError.textContent = `Email should be at least ${email.minLength} characters; you entered ${email.value.length}.`;
-//     }
-
-//     // Set the styling appropriately
-//     emailError.className = 'error active';
-
-// }
-
-
-// Loop Start 
-
-
 let numberOfInputs = document.querySelectorAll(".form-input").length;
 let numberOfSpans = document.querySelectorAll(".error").length;
 
@@ -96,18 +41,33 @@ for (let i = 0; i < numberOfInputs; i++) {
 
     function showError() {
 
-        if (inputFields.validity.valueMissing) {
-            // If the field is empty,
-            // display the following error message.
-            errorFields.textContent = 'You need to enter an e-mail address.';
-        } else if (inputFields.validity.typeMismatch) {
-            // If the field doesn't contain an email address,
-            // display the following error message.
-            errorFields.textContent = 'Entered value needs to be an e-mail address.';
-        } else if (inputFields.validity.tooShort) {
-            // If the data is too short,
-            // display the following error message.
-            errorFields.textContent = `Email should be at least ${email.minLength} characters; you entered ${email.value.length}.`;
+        if (inputFields.name == "first") {
+            errorFields.textContent = "First Name cannot be empty";
+        }
+
+        else if (inputFields.name == "last") {
+            errorFields.textContent = "Last Name cannot be empty";
+        }
+
+        else if (inputFields.name == "mail") {
+
+            if (inputFields.validity.valueMissing) {
+                // If the field is empty,
+                // display the following error message.
+                errorFields.textContent = 'You need to enter an e-mail address.';
+            } else if (inputFields.validity.typeMismatch) {
+                // If the field doesn't contain an email address,
+                // display the following error message.
+                errorFields.textContent = 'Entered value needs to be an e-mail address.';
+            } else if (inputFields.validity.tooShort) {
+                // If the data is too short,
+                // display the following error message.
+                errorFields.textContent = `Email should be at least ${email.minLength} characters; you entered ${email.value.length}.`;
+            }
+        }
+
+        else if (inputFields.name == "password") {
+            errorFields.textContent = "Passoword cannot be empty";
         }
 
         // Set the styling appropriately
